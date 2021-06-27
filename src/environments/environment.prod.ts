ebe2185cb84e3d4a36ed7db4f14a0bb64754e6ed
Oscar GUERIN <oscar.guerin@witty-services.fr>
2021-06-27 15:03:54 +0200

    release: v1.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment: any = {
   production: true,
   spotify: {
-    clientId: 'TODO',
-    redirectUri: 'TODO',
+    url: 'https://api.spotify.com/v1',
+    clientId: '2eba94e8d79e4c4f9c5f0b3b34e63310',
+    redirectUri: 'https://songuesser.web.app/callback',
+  },
+  api: {
+    url: 'https://us-central1-songuesser.cloudfunctions.net/api'
   }
 };
